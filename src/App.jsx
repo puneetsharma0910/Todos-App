4ee91630c8a625e5ef043f807e224c35f345807e
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-02-20 01:50:30 +0530

    Added Add/Delete/Edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,65 @@
-import { useState } from 'react'
-import Navbar from './components/navbar'
-
-import './App.css'
+import { useState } from "react";
+import Navbar from "./components/navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, settodo] = useState("");
+  const [todos, settodos] = useState([]);
+  const handleEdit = () => {};
+  const handledelete = () => {};
+  const handleadd = () => {
+    settodos([...todos, { todo, isCompleted: false }]);
+    settodo("");
+  };
 
+  const handlechange = (e) => {
+    settodo(e.target.value);
+  };
   return (
     <>
-     <Navbar/>
-     <div className="container">
-      <div className="bg-red-600">i am red</div>
-     </div>
+      <Navbar />
+      <div className="bg-slate-500 mx-auto max-w-[1400px] my-6 p-4 rounded-xl text-white  min-h-[70vh]  ">
+        <div className="addtodo text-white text-xl font-bold mb-7  ">
+          <h2>Add a todo</h2>
+          <input
+            onChange={handlechange}
+            value={todo}
+            className="rounded-md w-5/12 text-black"
+            type="text"
+          />
+          <button
+            onClick={handleadd}
+            className="bg-gray-600 py-1 p-2 mx-5  text-small
+           rounded-lg font-bold hover:bg-gray-700 "
+          >
+            Add
+          </button>
+        </div>
+        <h1 className="text-white text-xl font-bold ">Your Todos</h1>
+        {todos.map((item) => {
+          return (
+            <div className="todo flex my-3 justify-between w-2/4">
+              <input type="checkbox" value={item.isCompleted} />
+              <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
+              <div className="buttons">
+                <button
+                  onClick={handleEdit}
+                  className="bg-gray-600 py-1 p-2 mx-5 rounded-lg font-bold hover:bg-gray-700 "
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={handledelete}
+                  className="bg-gray-600 py-1 p-2 mx-5 rounded-lg font-bold hover:bg-gray-700 "
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div >
-      <nav className='flex bg-slate-500 row '>
-        <ul>
-            <li>Home</li>
-            <li>Your todos</li>
+      <nav className='flex bg-slate-600 justify-between mx-auto py-3 '>
+        <div>
+          <span className='font-bold text-3xl text-white cursor-pointer mx-9'>iTask</span>
+        </div>
+        <ul className='flex text-white gap-7  mx-9 '>
+            <li className='hover:font-bold cursor-pointer transition-all'>Home</li>
+            <li hover:font-bold cursor-pointer transition-all>Your todos</li>
         </ul>
       </nav>
     </div>
